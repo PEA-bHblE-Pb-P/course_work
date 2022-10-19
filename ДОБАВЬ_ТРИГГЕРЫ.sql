03d3b105b20b26ae45466ce0194b02ea7a0a3e12
kamilchulakov <mr1english4@gmail.com>
2022-10-19 12:58:24 +0300

    АПДЕЙТЫ ДЛЯ РОМАНА

--- ДОБАВЬ_ТРИГГЕРЫ.sql
+++ ДОБАВЬ_ТРИГГЕРЫ.sql
@@ -3,10 +3,12 @@ DROP TRIGGER IF EXISTS very_hard_trigger ON character;
 
 CREATE OR REPLACE FUNCTION validateAge() RETURNS TRIGGER AS $BODY$
 BEGIN
-    IF new.age_min >= 18 AND new.age_min <= new.age_max THEN
-        RETURN NEW;
+    IF new.age_min < 18 THEN
+        RAISE EXCEPTION 'Можно предпочитать только персонажей, которым строго не меньше 18 лет';
+    ELSEIF new.age_min <= new.age_max THEN
+        RAISE EXCEPTION 'Возрастные границы указаны неверно.';
     ELSE
-        RAISE EXCEPTION 'Возрастные границы указаны неверно';
+        RETURN NEW;
     END IF;
 END;
 $BODY$ LANGUAGE plpgsql;
