e57b432601adb2e47bcfc231c919d18024430b18
kamilchulakov <mr1english4@gmail.com>
2022-11-11 23:30:34 +0300

    FRONTEND-PLAYGROUND: Install eslint, prettier, pinia

---
+++ playground/what-we-do-in-the-shadows/.eslintrc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: ["eslint:recommended", "plugin:vue/vue3-recommended", "prettier"],
+  rules: {
+    // override/add rules settings here, such as:
+    // 'vue/no-unused-vars': 'error'
+  },
+};
