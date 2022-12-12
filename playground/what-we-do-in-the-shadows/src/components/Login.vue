0d2c54e1594975f4b3196e7424902f3cf10d9e13
kamilchulakov <mr1english4@gmail.com>
2022-12-12 22:00:20 +0300

    FRONTEND: login template

--- playground/what-we-do-in-the-shadows/src/components/Login.vue
+++ playground/what-we-do-in-the-shadows/src/components/Login.vue
@@ -1,3 +1,37 @@
 <template>
-  <h1 class="font-thin leading-tight text-8xl">LOGIN FORM</h1>
+  <div class="grid place-items-center">
+    <form>
+      <div class="mt-10 mb-6">
+        <input
+            type="text"
+            class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+            placeholder="Email address"
+        />
+      </div>
+      <div class="mb-6">
+        <input
+            type="password"
+            class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+            placeholder="Password"
+        />
+      </div>
+
+      <div class="flex justify-between items-center mb-6">
+        <div class="form-group form-check">
+          <input
+              type="checkbox"
+              class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-black checked:bg-black focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+              id="exampleCheck3"
+              checked
+          />
+          <label class="form-check-label inline-block text-gray-800" for="exampleCheck2">
+            Remember me
+          </label>
+        </div>
+      </div>
+      <button class="inline-block px-7 py-3 bg-gray-500 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-gray-600 hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" type="submit">LOGIN</button>
+      <button class="inline-block mx-2 px-7 py-3 bg-gray-400 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-gray-600 hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" type="submit">REGISTER</button>
+    </form>
+    <img class="mt-5" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fnypost.com%2Fwp-content%2Fuploads%2Fsites%2F2%2F2020%2F05%2Fwhat-we-do-in-the-shadows-02.jpeg%3Fquality%3D90%26strip%3Dall&f=1&nofb=1&ipt=5bc07f3a63ce1a346c438bcf3640e9225f03a834b4950454df7c48cd6454ec94&ipo=images"/>
+  </div>
 </template>
