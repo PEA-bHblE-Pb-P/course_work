44525e43192f5a55855beffda9f981b5fe2c8f68
Roman <roman.deev06@gmail.com>
2022-12-14 01:58:11 +0300

    cors

--- backend/ktor-backend/src/main/kotlin/ru/ifmo/cs/helios/s311693/plugins/HTTP.kt
+++ backend/ktor-backend/src/main/kotlin/ru/ifmo/cs/helios/s311693/plugins/HTTP.kt
@@ -13,6 +13,7 @@ fun Application.configureHTTP() {
         allowMethod(HttpMethod.Get)
         allowMethod(HttpMethod.Post)
         allowMethod(HttpMethod.Put)
+        allowMethod(HttpMethod.Options)
         allowMethod(HttpMethod.Delete)
         allowMethod(HttpMethod.Patch)
         allowHeader(HttpHeaders.Authorization)
