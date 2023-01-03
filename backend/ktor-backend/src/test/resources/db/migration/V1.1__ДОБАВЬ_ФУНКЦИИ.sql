1e87205d05dc48ab8335eaf49b08b280b42ad405
kamilchulakov <mr1english4@gmail.com>
2023-01-03 11:29:20 +0300

    found bug in go for fight

--- backend/ktor-backend/src/test/resources/db/migration/V1.1__ДОБАВЬ_ФУНКЦИИ.sql
+++ backend/ktor-backend/src/test/resources/db/migration/V1.1__ДОБАВЬ_ФУНКЦИИ.sql
@@ -191,10 +191,12 @@ END;
 $$ language plpgsql;
 
 
-create or replace function hunter_go_to_for_fight(hunter_id int, target_location_id int) RETURNS void as
+create function hunter_go_to_for_fight(hunter_id integer, target_location_id integer) returns void
+    language plpgsql
+as
 $$
 declare
-    vampire_type          integer;
+vampire_type          integer;
     vampire_cnt           integer;
     vamp_blood_percentage integer;
 begin
@@ -203,30 +205,30 @@ begin
     RAISE NOTICE 'Охотник на вамиров id=% вышел на охоту', hunter_id;
 
     perform go_to_location_by_id(hunter_id, target_location_id);
-    select id from type where name = 'вампир' INTO vampire_type;
-    select COUNT(*) from character_type_nearby(hunter_id, 1) into vampire_cnt;
-    RAISE NOTICE 'Вампиров рядом: %', vampire_cnt;
+select id from type where name = 'вампир' INTO vampire_type;
+select COUNT(*) from character_type_nearby(hunter_id, 1) into vampire_cnt;
+RAISE NOTICE 'Вампиров рядом: %', vampire_cnt;
     if vampire_cnt > 1 then
         perform kill(hunter_id, (select id from character_type_nearby(hunter_id, 1) LIMIT 1),
                      'Охотник проиграл вампиру и умер');
         RAISE NOTICE 'Охотник проиграл вампиру и умер';
-    elseif vampire_cnt == 1 then
-        select blood_percentage
-        from character
-        where character = (select id from character_type_nearby(hunter_id, 1))
-        into vamp_blood_percentage;
-        if vamp_blood_percentage != 100 then
+    elseif vampire_cnt = 1 then
+select blood_percentage
+from character c
+where c.id = (select id from character_type_nearby(hunter_id, 1))
+into vamp_blood_percentage;
+if vamp_blood_percentage != 100 then
             perform kill((select id from character_type_nearby(hunter_id, 1) LIMIT 1), hunter_id,
                          'Вампир слаб и его убил охотник');
             RAISE NOTICE 'Вампир слаб и его убил охотник';
-        else
+else
             perform kill((select id from character_type_nearby(1, hunter_id) LIMIT 1), hunter_id,
                          'Вампир силён и убил охотника');
             RAISE NOTICE 'Вампир силён и убил охотника';
-        end if;
-    else
+end if;
+else
         RAISE NOTICE 'Охотник не нашёл вампиров';
-    end if;
+end if;
 
 end ;
-$$ language plpgsql;
+$$;
