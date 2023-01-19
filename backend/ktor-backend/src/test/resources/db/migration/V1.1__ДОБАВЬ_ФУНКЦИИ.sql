07635001d29db84d87848b44c3d4177835522585
kamilchulakov <mr1english4@gmail.com>
2023-01-19 16:25:22 +0300

    fixes

--- backend/ktor-backend/src/test/resources/db/migration/V1.1__ДОБАВЬ_ФУНКЦИИ.sql
+++ backend/ktor-backend/src/test/resources/db/migration/V1.1__ДОБАВЬ_ФУНКЦИИ.sql
@@ -220,7 +220,7 @@ declare
 begin
     perform verify_character_exists(hunter_id);
     perform verify_character_alive(hunter_id);
-    assert (select type_id from character where id = hunter_id) !=
+    assert (select type_id from character where id = hunter_id) =
            (select id from type where name = 'охотник на вампиров'), 'check hunter type';
     RAISE NOTICE 'Охотник на вамиров id=% вышел на охоту', hunter_id;
 
